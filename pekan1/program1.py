2bd4edc3a0d0d5f0a258511d1b56a511f0b3755a
muyidann <myiwanumarhudan@gmail.com>
2026-09-08 16:26:44 +0700

    2026_alpro

---
+++ pekan1/program1.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("program ini menghasilkan")
+print("empat baris output")
+print("/ \\ // \\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n" + "M.Yiwanu Marhudan dan 2611533003")
