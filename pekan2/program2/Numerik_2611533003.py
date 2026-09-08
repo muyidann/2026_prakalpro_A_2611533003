6390bd0fd441990d3aad5d93df00d93f43c35d88
muyidann <myiwanumarhudan@gmail.com>
2026-09-08 09:12:11 +0700

    pekan 2

---
+++ pekan2/program2/Numerik_2611533003.py
@@ -0,0 +1,11 @@
+#create a variabel with integer value.
+a_3003=100
+print("The type of variable having value",a_3003,"is",type(a_3003))
+
+#create a variable with float value.
+b_3003=10.2345
+print("The type of variable having value",b_3003,"is",type(b_3003))
+
+#create a variable with complex value
+c_3003=100+3j
+print("The type of variable having value",c_3003,"is",type(c_3003))
---
+++ pekan2/program2/String_2611533003.py
@@ -0,0 +1,14 @@
+#nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
+#ganti programnya dan data masing2
+nama_3003 = "M. Yiwanu Marhudan_3003"
+jenis_kelamin_3003 = 'L'
+alamat_3003 = """
+    pesisir selatan
+    kec.silaut
+    silaut 1
+"""
+agama_3003 = 'islam'
+print("nama saya adalah: ", nama_3003)
+print("Jenis kelamin saya adalah: ", jenis_kelamin_3003)
+print("Alamat saya adalah: ", alamat_3003)
+print("Agama saya adalah: ", agama_3003)
