4e9254aad8037a1597c2ad14da92407d01df5b3f
muyidann <myiwanumarhudan@gmail.com>
2026-09-08 18:24:57 +0700

    Add biodata script for student information

--- pekan1/Laprak1/Tugas1_2611533003.py
+++
@@ -1,6 +0,0 @@
-print("=== BIODATA MAHASISWA ===")
-print("Nama    : M. Yiwanu Marhudan")
-print("NIM     : 2611533003")
-print("Jurusan : Informatika")
-print("Kelas   : IF-A")
-print("Hobi    : Membaca Komik")
