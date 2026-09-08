f36691ecf2fce7f311e444de300b1b61bb930557
muyidann <myiwanumarhudan@gmail.com>
2026-09-08 09:24:35 +0700

    pekan 2

---
+++ pekan2/Boolean_2611533003.py
@@ -0,0 +1,15 @@
+#Nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
+# Deklarasi variabel dengan tipe data boolean
+is_lulus_3003 = True
+is_cumlaide_3003 = False
+
+# Menggukan Boolean
+nilai_3003 = 85
+batas_lulus_3003 = 75
+
+# Menentukan nilai Boolean dari kondisi
+status_kelulusan_3003 = nilai_3003 >= batas_lulus_3003
+
+print("=== Check Kelulusan ===")
+print("Nilai:", nilai_3003)
+print("Apakah lulus:", status_kelulusan_3003)
